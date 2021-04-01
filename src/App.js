4facb1e108175f6dd788c980eaa57260ad54113c
Cody Sullivan <sullivan.cody7@gmail.com>
2021-03-31 18:07:24 -0600

    Parallax Samples

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import React from 'react'
-import { Parallax } from 'react-parallax'
+import React from 'react';
+import { Parallax } from 'react-parallax';
+import Sun from './bgSun.png';
+
+const inlinestyle = {
+  background: '#fff',
+  left: '50%',
+  top: '50%',
+  position: 'absolute',
+  padding: '20px',
+  transform: 'translate(-50%, -50%)',
+}
 
 function App() {
   return (
-    <div className="App">
-      <Parallax>
-        <div></div>
+    <div className="App" style={{ textAlign: 'center' }}>
+      <Parallax bgImage="/assets/images/SiteBG1.jpg" bgImageAlt="first BG">
+        <div style = {{height: 850}}>
+          <div style={inlinestyle}> HTML inside the parallax</div>
+        </div>
       </Parallax>
+      <h1>| | |</h1>
+      <Parallax bgImage="/assets/images/SiteBG2.jpg" bgImageAlt="second BG" blur={{ min: -1, max: 3 }}>
+        <div style = {{height: 850}}>
+          <div style={inlinestyle}>Dynamic Blur</div>
+        </div>
+      </Parallax>
+      <h1>| | |</h1>
+      <Parallax bgImage="/assets/images/bgStars.jpg" bgImageAlt="third BG" strength={-200}>
+        <div style = {{height: 850}}>
+          <div style={inlinestyle}>Reverse Direction</div>
+        </div>
+      </Parallax>
+      <h1>| | |</h1>
+      <Parallax 
+        bgImage="/assets/images/SiteBG1.jpg" 
+        bgImageAlt="third BG" 
+        strength={200}
+        renderLayer={percentage => (
+          <div 
+          style={{ 
+            width: '37%',
+            height: '590px',
+            position: 'absolute',
+            backgroundImage: "url("+ Sun + ")",
+            left: '50%',
+            top: '30%',
+            transform: `translate(-50%, -50%) scale(${percentage + 2})`,
+            opacity: '0.9',
+           }}>
+           {console.log(percentage)}
+          </div>
+        )}
+      >
+        <div style = {{height: 850}}>
+          <div style={inlinestyle}>Render Prop</div>
+        </div>
+      </Parallax>
+      <div style={{height: '100vh' }}></div>
     </div>
   )
 }
